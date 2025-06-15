c531e0104220b7a2a20f1a229844cb03a535fdae
koikeasuka <akoike@big-gate.co.jp>
2025-06-15 19:14:34 +0900

    Add: postにuser_id追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_15_061143) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_15_101216) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_bin", force: :cascade do |t|
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_bin", force: :cascade do |t|
@@ -30,4 +32,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_15_061143) do
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
