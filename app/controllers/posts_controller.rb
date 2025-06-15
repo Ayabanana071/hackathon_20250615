7745fc9796cdca6c0642fb7bd33ae370c819bd0b
koikeasuka <akoike@big-gate.co.jp>
2025-06-15 19:56:56 +0900

    Fix: タイポ修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[show]
   def index
     @q = Post.ransack(params[:q])
-    @posts = @q.result(distinct: true).includes.order(created_at: :desc).page(params[:page]).per(10)
+    @posts = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def show; end
