ae3cf7ccd927452953cfb8aa15f6a24ded3c8ae1
Tomonori <150645282+Tomo-akm@users.noreply.github.com>
2025-06-15 18:12:40 +0900

    faet: 各投稿の部分一致で検索を実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   validates :content, presence: true
   validates :content, length: { maximum: 280 }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["content"]
+  end
 end
