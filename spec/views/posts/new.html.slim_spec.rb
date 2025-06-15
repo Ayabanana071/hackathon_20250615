6349e92908bce349f55b439e231a6c64ddea3c4c
minami-asato <m.asato@churadata.okinawa>
2025-06-15 18:40:44 +0900

    refactor: rubocopの実行

--- spec/views/posts/new.html.slim_spec.rb
+++ spec/views/posts/new.html.slim_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "posts/new", type: :view do
-  before(:each) do
+RSpec.describe 'posts/new', type: :view do
+  before do
     assign(:post, Post.new(
-      content: "MyText"
-    ))
+                    content: 'MyText'
+                  ))
   end
 
-  it "renders new post form" do
+  it 'renders new post form' do
     render
 
-    assert_select "form[action=?][method=?]", posts_path, "post" do
-
-      assert_select "textarea[name=?]", "post[content]"
+    assert_select 'form[action=?][method=?]', posts_path, 'post' do
+      assert_select 'textarea[name=?]', 'post[content]'
     end
   end
 end
