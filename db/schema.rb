d9b0d4acbb295ac8747dd02d9403886f258d88d5
minami-asato <m.asato@churadata.okinawa>
2025-06-15 18:24:43 +0900

    fix: コンフリ対応

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_15_053826) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_15_061143) do
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_bin", force: :cascade do |t|
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_bin", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
