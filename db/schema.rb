0d45132c4bf542ce87ccbc8941f0d01f51ed7a09
Tomonori <150645282+Tomo-akm@users.noreply.github.com>
2025-06-15 11:21:14 +0900

    feat: scaffoldで投稿機能(index,new,crete,show)を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_15_015207) do
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_bin", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
